deb59f0e717f5a3d3a60abc9b27a3a0da56c3986
jrtaylor <josh.taylor@commercebank.com>
2019-12-03 14:58:10 -0600

    refactor(webpack): refactored gulp to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,36 @@
-{
-}
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'vue-user-timeout.min.js',
+    library: 'VueUserTimeout',
+    libraryTarget: 'umd',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // Must be set to true if using source-maps in production
+        terserOptions: {
+          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+        },
+      }),
+    ],
+  },
+};
